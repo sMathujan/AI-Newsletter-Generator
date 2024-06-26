6a96de0fec25ee415f22eddf4449edf3a2938d50
sMathujan <sivananthanmathujan@gmail.com>
2024-06-26 14:30:26 +0530

    crew.py file has been updated.

--- src/newsletter_crew/crew.py
+++ src/newsletter_crew/crew.py
@@ -1,11 +1,11 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-
-# Uncomment the following line to use an example of a custom tool
-# from newsletter_crew.tools.custom_tool import MyCustomTool
-
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
+from newsletter_crew.tools.research import SearchAndContents, FindSimilar, GetContents
+from langchain_anthropic import ChatAnthropic
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+from datetime import datetime
+import os
 
 @CrewBase
 class NewsletterCrewCrew():
@@ -13,35 +13,67 @@ class NewsletterCrewCrew():
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
+
+	def llm(self):
+		llm = ChatAnthropic(model_name="claude-3-sonnet-20240229", max_tokens=4096)
+		# llm = ChatGroq(model="llama3-70b-8192")
+        # llm = ChatGroq(model="mixtral-8x7b-32768")
+        # llm = ChatGoogleGenerativeAI(google_api_key=os.getenv("GOOGLE_API_KEY"))
+		return llm
+        
+
 	@agent
 	def researcher(self) -> Agent:
 		return Agent(
 			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
+			tools=[SearchAndContents(), FindSimilar(), GetContents()],
+			verbose=True,
+			llm=self.llm(),
 		)
 
 	@agent
-	def reporting_analyst(self) -> Agent:
+	def editor(self) -> Agent:
 		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
+			config=self.agents_config["editor"],
+            verbose=True,
+            tools=[SearchAndContents(), FindSimilar(), GetContents()],
+            llm=self.llm(),
 		)
+	
+	@agent
+	def designer(self) -> Agent:
+		return Agent(
+			config=self.agents_config["designer"],
+            verbose=True,
+            allow_delegation=False,
+            llm=self.llm(),
+		)
+	
 
 	@task
 	def research_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['research_task'],
-			agent=self.researcher()
+			agent=self.researcher(),
+			output_file=f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_research_task.md",
 		)
 
 	@task
-	def reporting_task(self) -> Task:
+	def edit_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['reporting_task'],
-			agent=self.reporting_analyst(),
-			output_file='report.md'
+			config=self.tasks_config["edit_task"],
+            agent=self.editor(),
+            output_file=f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_edit_task.md",
 		)
+	
+	@task
+	def newsletter_task(self) -> Task:
+		return Task(
+			config=self.tasks_config["newsletter_task"],
+            agent=self.designer(),
+            output_file=f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_newsletter_task.html",
+		)
+	
 
 	@crew
 	def crew(self) -> Crew:
@@ -53,3 +85,4 @@ class NewsletterCrewCrew():
 			verbose=2,
 			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
+	
