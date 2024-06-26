e87dbf96c6549ce47c273bb5de11fa612563a908
sMathujan <sivananthanmathujan@gmail.com>
2024-06-26 14:37:25 +0530

    main.py has been updated.

--- src/newsletter_crew/main.py
+++ src/newsletter_crew/main.py
@@ -3,10 +3,19 @@ import sys
 from newsletter_crew.crew import NewsletterCrewCrew
 
 
+def load_html_template(): 
+    with open('src/newsletter_crew/config/newsletter_template.html', 'r') as file:
+        html_template = file.read()
+        
+    return html_template
+
+
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'topic': 'AI LLMs'
+        'topic': input('Enter the topic for yout newsletter: '),
+        'personal_message': input('Enter a personal message for your newsletter: '),
+        'html_template': load_html_template()
     }
     NewsletterCrewCrew().crew().kickoff(inputs=inputs)
 
